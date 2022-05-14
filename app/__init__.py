99921676c21450c444561678037b610b60bd6b4a
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-15 00:53:00 +0300

    import the configure_request() function from the request.py file. We call the function and pass the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,25 @@ from config import DevConfig
 from config import config_options
 from flask_bootstrap import Bootstrap
 
-
+bootstrap = Bootstrap()
 def create_app(config_name):
   app = Flask(__name__)
 
 
-# Creating the app configurations
+  # Creating the app configurations
   app.config.from_object(config_options)
+
+  # Registering the blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  # setting config
+  from .requests import configure_request
+  configure_request(app)
+
+  return app
+
+    
     
     
 # app = Flask(__name__,instance_relative_config=True)
@@ -20,5 +32,6 @@ def create_app(config_name):
 
 
 # Initializing Flask Extensions
-bootstrap = Bootstrap()
+
 # from .main import views
+
