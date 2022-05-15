5278a0f78a8e48bf90187e2808566dad0f8fba57
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 01:53:08 +0300

    profile.html and navbar.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from app.requests import get_quote
 from app.main import main
 # from models import Quote
 from flask_login import login_required
+from ..models import Reviews, User
+
 
 # Views
 @main.route('/')
@@ -16,3 +18,12 @@ def index():
    
     title = 'Bekitas Blog'
     return render_template('index.html',  title = title, Quote=Quote)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
