747c9a3c058d2fc8d0ea7e3d9d1d706ad6f2d426
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 01:06:45 +0300

    it will create a form.py file and create a registration form

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from . import views,forms
 
 
 bootstrap = Bootstrap()
