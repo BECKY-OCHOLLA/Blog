1d634480d3643c861efbd8c39bffcd66947eb7b1
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 00:05:50 +0300

    it will create a user model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from  . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Quote:
     '''
@@ -9,4 +10,31 @@ class Quote:
         
         self.author = author
         self.quote = quote
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    secure_password = db.Column(db.String(255),nullable = False)
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
         
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'User {self.username}'
