be211a86f915804f02a63b8cbe0226d6c65e1d4d
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 01:16:09 +0300

    it will add a login form and validation fields

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,6 +5,7 @@ from ..models import User
 from wtforms import ValidationError
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
+
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
@@ -19,3 +20,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
