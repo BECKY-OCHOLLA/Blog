ef933f3412fff7a888cabcb20d2cdb3ce32591b0
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 01:32:47 +0300

    it will create a login/logout/subscribe routes in the view.py file in the auth folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,3 +19,49 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form=form)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Scribble login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/subscribe', methods=['GET','POST'])
+def subscriber():
+    subscriber_form = SubscriberForm()
+    if subscriber_form.validate_on_submit():
+
+        blogs = Post.query.order_by(Post.date_created.desc()).all()
+        subscriber = Post.query.all()
+
+        blogs = Post.query.all()
+
+        subscriber= Subscriber(email=subscriber_form.email.data,name = subscriber_form.name.data)
+
+        db.session.add(subscriber)
+        db.session.commit()
+
+        mail_message("Welcome to BlogTech.com","email/welcome_subscriber",subscriber.email,subscriber=subscriber)
+
+        title= "BlogTech.com"
+       
+        return redirect(url_for('main.blog', title=title, blogs=blogs, subscriber_form=subscriber_form))
+
+
+    return render_template('subscribe.html',subscriber_form=subscriber_form)  
