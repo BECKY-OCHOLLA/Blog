47b95656a3634b41163d7465da53d9d0da4152c4
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-16 11:48:29 +0300

    it will create a update_profile view function that takes in a username, and instantiates the UpdateProfile form class.

---
+++ app/main/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
+from wtforms.validators import Required, Email
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Author', validators=[Required()])
+    post = TextAreaField('Blog', validators=[Required()])
+    submit = SubmitField('Post')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Post')
+
+
+class Vote(FlaskForm):
+    submit = SelectField('Like')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('bio', validators=[Required()])
+    submit = SubmitField('Post')
--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,8 @@ from app.main import main
 # from models import Quote
 from flask_login import login_required
 from ..models import Reviews, User
+from .forms import ReviewForm,UpdateProfile
+from .. import db
 
 
 # Views
@@ -27,3 +29,22 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
